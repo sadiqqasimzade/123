dd256ba6c74430e2109062dca32dc71a47c6be48
Sadig Gasimzada <tu6w5xs1c@aztu.local>
2022-03-14 18:56:35 +0400

    .

---
+++ example/example/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon gun = new Weapon(10, 0, 4, true);
+            int choise = -1;
+            while (choise!=-1)
+            {
+                Console.WriteLine("Choise:");
+                choise = Convert.ToInt32(Console.ReadLine());
+
+                switch (choise)
+                {
+                    case 0: {
+                            
+                        
+                        }break;
+                    case 1:
+                        {
+
+                        }break;
+                    case 2:
+                        {
+
+                        }break;
+                    case 3:
+                        {
+
+                        }
+                        break;
+                    case 4:
+                        {
+
+                        }
+                        break;
+                    case 5:
+                        {
+
+                        }
+                        break;
+                    case 6:
+                        {
+
+                        }
+                        break;
+                    default:
+                        {
+                            
+                        }break;
+                }
+            }
+        }
+    }
+}
---
+++ example/example/Weapon.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace example
+{
+    class Weapon
+    {
+        public int bulletCap;
+        public int bulletCount;
+        public double time;
+        public bool auto = true;
+
+        public Weapon(int bulletCap,int bulletCount,double time,bool auto)
+        {
+            this.bulletCap = bulletCap;
+            this.bulletCount = bulletCount;
+            this.time = time;
+            this.auto = auto;
+            if (bulletCount > bulletCap) Console.WriteLine("WARNING:WRONG DATA:bulletcount cant be greater than bulletcap");
+        }
+
+
+
+        public  void Shot()
+        {
+            if (bulletCount <= 0)
+            {
+                Console.WriteLine("no bullet left");
+            }
+            else if (auto==true)
+            {
+                Console.WriteLine("pew");
+                bulletCount--;
+                if (bulletCount > 0)
+                {
+                    bulletCount--;
+                    Console.WriteLine("pew");
+                }
+                else
+                {
+                    Console.WriteLine("no bullet left");
+                }
+            }
+           else
+            {
+                bulletCount--;
+                Console.WriteLine("pew");
+            }
+            
+            
+
+        }
+        public  void Fire()
+        {
+            double shotedbullets=0;
+            if(bulletCount==0)
+            {
+                Console.WriteLine("no bullet left");
+                return;
+            }
+            while(bulletCount>0)
+            {
+                Console.WriteLine("Shot");
+                shotedbullets++;
+            }
+            shotedbullets *= (bulletCap / time);
+            Console.WriteLine($"no bullet left.time:{shotedbullets}");
+        }
+
+        public  void GetRemainBulletCount()
+        {
+            Console.WriteLine("Bullets left:"+bulletCount);
+        }
+
+        public  void Reload()
+        {
+            if (bulletCap - bulletCount == 0) Console.WriteLine("ammo is full");
+            else
+            {
+                Console.WriteLine($"{bulletCap - bulletCount}:bullet added");
+                bulletCount = bulletCap;
+            }
+        }
+        public void Info()
+        {
+
+        }
+
+    }
+}
