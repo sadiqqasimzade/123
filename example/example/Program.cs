c2200afbf5c100dfd6f0022ec65fc75c7670cd10
sadiqqasimzade <tu6w5xs1c@code.edu.az>
2022-03-15 10:57:29 +0400

    still working

--- example/example/Program.cs
+++ example/example/Program.cs
@@ -1,55 +1,64 @@
 ﻿using System;
-namespace example
+namespace example  
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Weapon gun = new Weapon(10, 0, 4, true);
-            int choise = -1;
-            while (choise!=-1)
-            {
-                Console.WriteLine("Choise:");
-                choise = Convert.ToInt32(Console.ReadLine());
+            Console.OutputEncoding = System.Text.Encoding.GetEncoding("UTF-16");
+            Console.Write("bulletcap:");
+            int bulletcap = Convert.ToInt16(Console.ReadLine());
+            Console.Write("bulletcount:");
+            int bulletcount = Convert.ToInt16(Console.ReadLine());
+            Console.Write("time:");
+            double time = Convert.ToDouble(Console.ReadLine());
+            bool auto = true;
+            
 
+            Weapon gun = new Weapon(bulletcap, bulletcount, time, auto);
+            int choise = 0;
+            while (choise != 6)
+            {
+                Console.Write(@"
+0 - İnformasiya almaq üçün
+1 - Shoot metodu üçün
+2 - Fire metodu üçün
+3 - GetRemainBulletCount metodu üçün
+4 - Reload metodu üçün
+5 - ChangeFireMode metodu üçün
+6 - Proqramdan dayandirmaq üçün
+7 - Clear console
+Choise:");
+                choise = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("---------------------------");
                 switch (choise)
                 {
-                    case 0: {
-                            
-                        
-                        }break;
+                    case 0:
+                        gun.Info();
+                        break;
                     case 1:
-                        {
-
-                        }break;
+                        gun.Shoot();
+                        break;
                     case 2:
-                        {
-
-                        }break;
+                        gun.Fire();
+                        break;
                     case 3:
-                        {
-
-                        }
+                        gun.GetRemainBulletCount();
                         break;
                     case 4:
-                        {
-
-                        }
+                        gun.Reload();
                         break;
                     case 5:
-                        {
-
-                        }
+                        gun.ChangeFireMode();
                         break;
-                    case 6:
-                        {
-
-                        }
+                    case 6: break;
+                    case 7:
+                        Console.Clear();
                         break;
                     default:
-                        {
-                            
-                        }break;
+                        Console.WriteLine("Wrong input");
+
+                        break;
                 }
             }
         }
--- example/example/Weapon.cs
+++ example/example/Weapon.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace example
 {
@@ -9,26 +7,25 @@ namespace example
         public int bulletCap;
         public int bulletCount;
         public double time;
-        public bool auto = true;
+        public bool auto;
 
-        public Weapon(int bulletCap,int bulletCount,double time,bool auto)
+        public Weapon(int bulletCap, int bulletCount, double time, bool auto)
         {
             this.bulletCap = bulletCap;
             this.bulletCount = bulletCount;
             this.time = time;
             this.auto = auto;
-            if (bulletCount > bulletCap) Console.WriteLine("WARNING:WRONG DATA:bulletcount cant be greater than bulletcap");
+            InputChecker();
         }
 
 
 
-        public  void Shot()
+        public void Shoot()
         {
-            if (bulletCount <= 0)
-            {
+            if (bulletCount == 0)
                 Console.WriteLine("no bullet left");
-            }
-            else if (auto==true)
+            
+            else if (auto == true)
             {
                 Console.WriteLine("pew");
                 bulletCount--;
@@ -38,44 +35,58 @@ namespace example
                     Console.WriteLine("pew");
                 }
                 else
-                {
                     Console.WriteLine("no bullet left");
-                }
+                
             }
-           else
+            else
             {
                 bulletCount--;
                 Console.WriteLine("pew");
             }
-            
-            
 
         }
-        public  void Fire()
+        public void Fire()
         {
-            double shotedbullets=0;
-            if(bulletCount==0)
+            double cooldown = 1;//atislar arasinda fasile
+            double shotedbullets = 0;
+            if (bulletCount == 0)
             {
                 Console.WriteLine("no bullet left");
                 return;
             }
-            while(bulletCount>0)
+            if (auto)
             {
-                Console.WriteLine("Shot");
-                shotedbullets++;
+                while (bulletCount > 0)
+                {
+                    Console.Write("pew");
+                    bulletCount--;
+                    shotedbullets++;
+                }
             }
-            shotedbullets *= (bulletCap / time);
-            Console.WriteLine($"no bullet left.time:{shotedbullets}");
+            else
+            {
+                while (bulletCount > 0)
+                {
+                    Console.WriteLine("pew");
+                    bulletCount--;
+                    shotedbullets++;
+                }
+            }
+            if (auto == true) shotedbullets = ((time / bulletCap) * shotedbullets) + shotedbullets * cooldown;
+            else shotedbullets *= (time / bulletCap);
+            Console.WriteLine($"no bullet left.\ntime:{shotedbullets}sec");
         }
 
-        public  void GetRemainBulletCount()
+        public void GetRemainBulletCount()
         {
-            Console.WriteLine("Bullets left:"+bulletCount);
+            Console.WriteLine("Bullets need:" + (bulletCap-bulletCount));
         }
 
-        public  void Reload()
+        public void Reload()
         {
-            if (bulletCap - bulletCount == 0) Console.WriteLine("ammo is full");
+            if (bulletCap - bulletCount == 0)
+                Console.WriteLine("ammo is full");
+            
             else
             {
                 Console.WriteLine($"{bulletCap - bulletCount}:bullet added");
@@ -84,6 +95,35 @@ namespace example
         }
         public void Info()
         {
+            Console.WriteLine(@$"       Info
+BulletCap:{bulletCap}
+BulletCount:{bulletCount}
+FireTime:{time}
+Auto fire mode:{auto}");
+        }
+
+        public void ChangeFireMode()
+        {
+            auto = !auto;
+            Console.WriteLine("Mode changed");
+        }
+        public void InputChecker()
+        {
+            if(bulletCap<=0)
+            {
+                bulletCap = 10;
+                Console.WriteLine("WARNING:BULLET CAPACITY CANT BE 0 OR NEGATIVE.USING DEFAULT PARAMS:bullet capacity=10");
+            }
+            if(bulletCount>bulletCap)
+            {
+                bulletCount = 0;
+                Console.WriteLine("WARNING:BULLET AMOUT CANT BE GREATER THAN CAPACITY.USING DEFAULT PARAMS:bullet amout=0");
+            }
+            if(time<=0)
+            {
+                time = 10;
+                Console.WriteLine("WARNING:TIME CANT BE 0 OR NEGATIVE.USING DEFAULT PARAMS:time=10sec");
+            }
 
         }
 
